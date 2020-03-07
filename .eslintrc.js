fcf57e687380a1c092cf1eb658223070defb8b93
EMPcode <dev@empost.eu>
2020-03-07 23:11:53 +0200

    Add globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
     "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+        "SharedArrayBuffer": "readonly",
+        "AllEars": "readonly",
+	"sodium": "readonly"
     },
     "parserOptions": {
         "ecmaVersion": 2018
