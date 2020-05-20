9759086087c450311cd6e84448e74013dfe79574
EMPcode <dev@empost.eu>
2020-05-20 23:28:02 +0300

    Fade inbox table during update

--- modern/main.js
+++ modern/main.js
@@ -310,7 +310,11 @@ document.getElementById("btn_updt").onclick = function() {
 	btn.blur();
 
 	if (tab === "inbox") {
+		document.getElementById("tbl_inbox").style.opacity = 0.5;
+
 		ae.Message_Browse(0, function(successBrowse) {
+			document.getElementById("tbl_inbox").style.opacity = 1;
+
 			if (successBrowse) {
 				clearMessages();
 				addMessages();
