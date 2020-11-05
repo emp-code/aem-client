64cdaa780b378ac06b4447c6f0d350b48325bb2a
EMPcode <dev@empost.eu>
2020-11-05 13:16:39 +0200

    Add sodium_init()

--- Fetcher/main.c
+++ Fetcher/main.c
@@ -1,6 +1,8 @@
 #include "../all-ears.h"
 
 int main(void) {
+	if (sodium_init() != 0) {puts("Failed sodium_init()"); return EXIT_FAILURE;}
+
 //	allears_init(host, lenHost, usk);
 
 //	allears_address_lookup("test");
