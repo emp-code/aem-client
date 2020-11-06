66c499a33beb48a6032e51634352a7fbe5a10a12
EMPcode <dev@empost.eu>
2020-11-06 10:39:55 +0200

    Corrections to parentheses

--- Tester/main.c
+++ Tester/main.c
@@ -45,7 +45,7 @@ static int performTests(int * const retNum, const char onionId[56], const unsign
 	// User1
 	if (allears_init(onionId, spk, saltNm, key_user1) != 0) return -1;
 	(*retNum)++; if ((ret = allears_address_delete(UINT64_MAX)) < 0) return -1;
-	(*retNum)++; if ((ret = allears_account_delete(upk_user2) >= 0)) return -1;
+	(*retNum)++; if ((ret = allears_account_delete(upk_user2)) >= 0) return -1;
 	(*retNum)++; if ((ret = allears_account_update(upk_user2, 3)) >= 0) return -1;
 	(*retNum)++; if ((ret = allears_account_update(upk_user2, 1)) >= 0) return -1;
 	(*retNum)++; if ((ret = allears_account_update(upk_user1, 3)) >= 0) return -1;
@@ -66,7 +66,7 @@ static int performTests(int * const retNum, const char onionId[56], const unsign
 	// TODO: Delete a non-existing message
 	// TODO: Browse messages, verify corrrect message deleted
 	// TODO: Delete address
-	(*retNum)++; if ((ret = allears_account_delete(upk_user1) != 0)) return ret;
+	(*retNum)++; if ((ret = allears_account_delete(upk_user1)) != 0) return ret;
 
 	return 0;
 }
