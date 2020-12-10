c8a323e4bc651b921470f85a36e18130266e4a4f
EMPcode <dev@empost.eu>
2020-12-10 13:59:56 +0200

    Handle erroneous countrycodes

--- modern/main.js
+++ modern/main.js
@@ -299,7 +299,7 @@ function getCountryName(countryCode) {
 }
 
 function getCountryFlag(countryCode) {
-	return (countryCode == "??") ? "❔" : sodium.to_string(new Uint8Array([
+	return (!countryCode || countryCode.length !== 2 || countryCode == "??") ? "❔" : sodium.to_string(new Uint8Array([
 		240, 159, 135, 166 + countryCode.codePointAt(0) - 65,
 		240, 159, 135, 166 + countryCode.codePointAt(1) - 65
 	]));
