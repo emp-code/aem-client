4b8d7d57712c883e6214f8a968553035dea0d1fe
emp-code <dev@empost.eu>
2021-02-21 14:08:32 +0200

    Add ipBlacklisted functionality

--- modern/main.js
+++ modern/main.js
@@ -577,7 +577,7 @@ function displayMsg(isInt, num) {
 
 		document.getElementById("readmsg_country").textContent = getCountryFlag(cc);
 		document.getElementById("readmsg_country").title = getCountryName(cc);
-		document.getElementById("readmsg_ip").children[1].textContent = ae.GetExtMsgIp(num);
+		document.getElementById("readmsg_ip").children[1].textContent = ae.GetExtMsgIp(num) + (ae.GetExtMsgFlagIpBl(num) ? " ❗" : "");
 		document.getElementById("readmsg_rdns").children[0].textContent = ae.GetExtMsgRdns(num);
 		document.getElementById("readmsg_tls").children[0].textContent = ae.GetExtMsgTLS(num);
 		document.getElementById("readmsg_greet").children[0].textContent = ae.GetExtMsgGreet(num) + (ae.GetExtMsgFlagGrDm(num) ? " ✓" : "");
