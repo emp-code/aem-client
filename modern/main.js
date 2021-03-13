0491f1928cca18e274161b5b9a755db3a7557e7a
emp-code <dev@empost.eu>
2021-03-14 01:38:25 +0200

    Don't use pointer cursor on empty headers

--- modern/main.js
+++ modern/main.js
@@ -533,7 +533,7 @@ function displayMsg(isInt, num) {
 		document.getElementById("midright").children[2].appendChild(body);
 
 		document.getElementById("midright").children[1].textContent = ae.GetExtMsgTitle(num);
-		document.getElementById("midright").children[1].style.cursor = "pointer";
+		document.getElementById("midright").children[1].style.cursor = headers.textContent? "pointer" : "";
 		document.getElementById("midright").children[1].onclick = function() {
 			if (!headers.textContent) return;
 			showHeaders = !showHeaders;
