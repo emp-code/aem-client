eab4fcef77ff4076f6cbd7c22e25175bd1cd7b58
emp-code <dev@empost.eu>
2022-04-11 16:26:47 +0300

    Remove deleted addresses from APK list

--- modern/main.js
+++ modern/main.js
@@ -940,6 +940,15 @@ function deleteAddress(addr) {
 
 		document.getElementById("tbl_addrs").deleteRow(addressToDelete);
 		document.getElementById("write_from").remove(addressToDelete);
+
+		const apkList = document.getElementById("getapk_addr");
+		for (let i = 0; i < apkList.children.length; i++) {
+			if (apkList.children[i].value === addr) {
+				apkList.remove(i);
+				break;
+			}
+		}
+
 		updateAddressCounts();
 
 		ae.Private_Update(function(error2) {
