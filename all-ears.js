49d47ec3dfef87bd914dbc786dd4d4e620cf8572
emp-code <dev@empost.eu>
2022-04-13 12:13:39 +0300

    htmlIntMsg(): Cleaner output

--- all-ears.js
+++ all-ears.js
@@ -1620,7 +1620,13 @@ function AllEars(readyCallback) {
 
 	this.htmlIntMsg = function(num) {if(typeof(num)!=="number"){return;}
 		const msgDate = new Date((_intMsg[num].ts * 1000) + ((new Date().getTimezoneOffset()) * -60000)).toISOString().slice(0, 19).replace("T", " ");
-		const msg = "<html><body><pre>Date: " + msgDate + "\nFrom: " + _intMsg[num].from + "@" + _AEM_DOMAIN_EML + "\n  To: " + _intMsg[num].to + "@" + _AEM_DOMAIN_EML + "</pre><h1>" + _intMsg[num].title + "</h1><p>" + this.getIntMsgBody(num).replaceAll("\n", "<br>") + "</p></body></html>";
+		const msg = "<!doctype html><html><body>\n<pre>"
+		+ "\nDate: " + msgDate
+		+ "\nFrom: " + _intMsg[num].from + "@" + _AEM_DOMAIN_EML
+		+ "\n  To: " + _intMsg[num].to + "@" + _AEM_DOMAIN_EML
+		+ "\n</pre>\n<h1>" + _intMsg[num].title + "</h1>\n<p>\n"
+		+ this.getIntMsgBody(num).replaceAll("\n", "<br>") + "\n</p>\n</body></html>";
+
 		_downloadFile(_intMsg[num].title + ".html", new Blob([msg]));
 	};
 
